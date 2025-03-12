6a64b42eeacf4b9dc2e08cd2550fc45831e6cef9
Lewis <lewis.njuma@student.moringaschool.com>
2025-03-12 21:35:09 +0300

    control-flow

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let distance = 2501
+function scuberGreetingForFeet(distance){
+  if (distance <= 400) {
+   return ("This one is on me!");
+  }
+  else if(distance > 400 && distance <= 2000) {
+    return("That will be twenty bucks.");
+  }
+  else if(distance > 2000 && distance <= 2500) {
+    return("I will gladly take your thirty bucks.");
+  }
+  else
+  return("No can do.");
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+let town = "Pittsburgh";
+function ternaryCheckCity(town){
+  let result = town === "NYC" ? "Ok, sounds good." : "No go."
+  console.log(result);
+  return result
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+let tip = "thanks for everything";
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous" : return("Thank you so much.");  
+    break;
+
+    case "not as generous" : return("Thank you.");
+    break;
+
+    default : return("Bye.");
+  }
 }
